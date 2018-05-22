f87b525912ff3b66b008c1d3cbe0d8a8f0c2d5aa
Erik <emcp@whichdegree.co>
2018-05-22 21:09:04 +0200

    Upgrade to MAHRIO 1.5
    Required update in order to disable the TLS upgrade

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 process.env.NODE_URL="192.168.0.5"; //locate your own ip address
 process.env.NODE_PORT="6085";
-
+process.env.NODE_ENV="development";
 var counter = 0;
 require('mahrio').runServer( process.env, __dirname).then( function(server){
   server.route({
